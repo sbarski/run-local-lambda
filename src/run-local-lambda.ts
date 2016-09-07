dcd72f629e2b8c756998da11342c3ec8e1f2e23b
Stef Heyenrath <stef.heyenrath@gmail.com>
2016-09-07 18:37:01 +0000

    Convert to TypeScript
    Convert to TypeScript

--- src/run-local-lambda.ts
+++ src/run-local-lambda.ts
@@ -9,15 +9,26 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
-"use strict";
-var path = require('path');
-var settings = {
-    file: 'index.js',
-    event: null,
-    timeout: 3000,
-    handler: 'handler'
+
+import * as path from 'path';
+import { Context } from 'aws-lambda';
+
+export interface Settings {
+    file: string;
+    event?: string;
+    timeout: number;
+    handler: string;
+}
+
+let settings: Settings = {
+    file : 'index.js',
+    event : null,
+    timeout : 3000,
+    handler : 'handler'
 };
-(function () {
+
+(function() {
+
     /*
      * Q: Why not use commander to parse command line arguments? A. To reduce dependencies.
      *
@@ -30,100 +41,116 @@ var settings = {
      * E.g. node run-local-lambda (providing index.js and event.json are in the current directory)
      * E.g. node run-local-lambda --file index.js --event event.json --timeout 3 --memory 128 --handler handler
      */
-    function processArguments() {
-        process.argv.forEach(function (argument, index, array) {
+    function processArguments(): void {
+			process.argv.forEach(function(argument, index, array) {
             switch (argument) {
                 case '--file':
                     settings.file = array[index + 1];
-                    break;
+				break;
+
                 case '--event':
                     settings.event = array[index + 1];
-                    break;
+				break;
+
                 case '--timeout':
                     settings.timeout = parseInt(array[index + 1], 10) * 1000; // convert seconds to milliseconds
-                    break;
+				break;
+
                 case '--handler':
                     settings.handler = array[index + 1];
-                    break;
+                break;
+
                 default:
                     break;
             }
         });
     }
-    function createContext(functionName, timeout) {
-        var startTime = new Date();
-        var succeed = function (result) {
+
+	function createContext(functionName: string, timeout: number): Context {
+		let startTime = new Date();
+
+        let succeed = function(result){
             if (result !== undefined) {
                 console.log(JSON.stringify(result));
             }
+
             process.exit();
         };
-        var fail = function (error) {
+
+        let fail = function(error){
             if (error !== undefined) {
                 console.log(error);
             }
+
             process.exit(1);
         };
-        var done = function (error, result) {
+
+        let done = function(error, result){
             if (error === null) {
                 succeed(result);
-            }
-            else {
+            } else {
                 fail(error);
             }
         };
-        var getRemainingTimeInMillis = function () {
-            var currentTime = new Date();
-            return settings.timeout - (currentTime.getMilliseconds() - startTime.getMilliseconds());
+
+        let getRemainingTimeInMillis = function() { // Returns the approximate remaining execution time (before timeout occurs)
+			let currentTime = new Date();
+			return settings.timeout - (currentTime.getMilliseconds() - startTime.getMilliseconds());
         };
-        var hash = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
-        var context = {
-            awsRequestId: [hash.substr(0, 8), hash.substr(9, 4), hash.substr(12, 4), hash.substr(16, 4), hash.substr(20, 12)].join('-'),
-            logGroupName: '/aws/lambda/' + functionName,
-            logStreamName: (new Date()).toISOString().substr(0, 10).replace(/-/g, '/') + '/[$LATEST]' + hash,
+
+        let hash: string = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
+        let context: Context = {
+            awsRequestId : [ hash.substr(0, 8), hash.substr(9, 4), hash.substr(12, 4), hash.substr(16, 4), hash.substr(20, 12) ].join('-'),
+            logGroupName : '/aws/lambda/' + functionName,
+            logStreamName : (new Date()).toISOString().substr(0, 10).replace(/-/g, '/') + '/[$LATEST]' + hash,
             functionName: functionName,
             functionVersion: '1.0',
             invokedFunctionArn: 'arn:aws:lambda:aws-region:1234567890123:function:' + functionName,
-            memoryLimitInMB: 128,
+            memoryLimitInMB : 128,
             getRemainingTimeInMillis: getRemainingTimeInMillis,
             callbackWaitsForEmptyEventLoop: true,
             succeed: succeed,
             fail: fail,
             done: done
         };
+
         return context;
-    }
-    function callback(error, result) {
+	}
+
+    function callback(error: string | Error, result: any): void {
         if (error === undefined || error === null) {
             if (result !== undefined && result !== null) {
                 console.log(JSON.stringify(result));
             }
-        }
-        else {
+        } else {
             if (error instanceof Error) {
                 console.log({
-                    'errorMessage': error.message,
-                    'errorType': error.name,
-                    'stack': error.stack
+                    'errorMessage' : error.message,
+                    'errorType' : error.name,
+                    'stack' : error.stack
                 });
-            }
-            else {
+            } else {
                 console.log('errorMessage: ' + error);
             }
         }
+
         process.exit();
     }
+
     processArguments();
-    var event = settings.event != null ? require(path.resolve(settings.event, '.')) : null;
-    var context = createContext('func', settings.timeout);
-    var lambda = require(path.resolve(settings.file, '.'));
-    function execute() {
-        setTimeout(function () {
-            console.log('The function timed out after ' + settings.timeout + ' seconds');
-            process.exit();
-        }, settings.timeout);
-        lambda[settings.handler](event, context, callback);
-    }
-    ;
+
+    let event: any = settings.event != null ? require(path.resolve(settings.event, '.')) : null;
+    let context: Context = createContext('func', settings.timeout);
+    let lambda: any = require(path.resolve(settings.file, '.'));
+
+    function execute(): void {
+		setTimeout(function(){
+			console.log('The function timed out after ' + settings.timeout + ' seconds');
+			process.exit();
+		}, settings.timeout);
+
+		lambda[settings.handler](event, context, callback)
+	};
+
     execute();
 })();
